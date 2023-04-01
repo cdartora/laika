6a6d67248b073119dd42fd35dcdb60d799d570a5
Carlos Dartora <carlosdalsoler@gmail.com>
2023-04-01 17:19:10 -0300

    feat: :art: termina site

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LaikaLogo from "./assets/laika-logo.svg";
+import { useState } from "react";
+import { EnvelopeSimple, Clipboard } from "@phosphor-icons/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isClipboardSolid, setIsClipboardSolid] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ left: 0, top: 0 });
+
+  const mouseInside = () => {
+    setIsClipboardSolid(true);
+  };
+
+  const mouseOutside = () => {
+    setIsClipboardSolid(false);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText("laikaprodutora@gmail.com");
+  };
+
+  const trailEffect = (event) => {
+    const { clientX, clientY } = event;
+    setMousePosition({ left: clientX, top: clientY });
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div
+        className="page h-screen p-4 min-w-[350px]"
+        onMouseMove={trailEffect}
+      >
+        <div className="content max-w-md h-full flex-col mx-auto">
+          <div className="flex flex-col items-center mb-10 cursor-zoom-in">
+            <img
+              src={LaikaLogo}
+              alt="Logo da produtora Laika. Laika por extenso e estilizado."
+              className="w-44 mt-4 mx-4"
+            />
+            <div className="sub-title text-center text-sm underline underline-offset-2">
+              <p>luta artística independente & koletivo autoral</p>
+            </div>
+          </div>
+          <div className="contato">
+            <p className="mb-4">
+              Procuramos projetos e oportunidades para movimentar a cidade.
+            </p>
+            <h3 className="font-bold">Entre em contato:</h3>
+            <div className="e-mail-button-container flex gap-3 border-2 border-black justify-between p-1 overflow-x-auto">
+              <div className="flex gap-1">
+                <EnvelopeSimple size={32} />
+                <span>laikaprodutora@gmail.com</span>
+              </div>
+              <div>
+                <Clipboard
+                  size={32}
+                  weight={isClipboardSolid ? "regular" : "fill"}
+                  className="cursor-pointer"
+                  onMouseEnter={mouseInside}
+                  onMouseLeave={mouseOutside}
+                  onClick={copyToClipboard}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer text-xs flex justify-center">
+          feito com carinho, Laika. 2023
+        </div>
+        {/* <div
+          style={{ left: mousePosition.left, top: mousePosition.top }}
+          id="blob"
+          onMouseMove={trailEffect}
+        ></div> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
